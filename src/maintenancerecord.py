3e78fca9486d58ce3cf232532b247500349b1f2e
Ashigbui <ash.dzifa@gmail.com>
2026-08-07 23:32:28 +0000

    Add MaintenanceRecord class with initial methods

---
+++ src/maintenancerecord.py
@@ -0,0 +1,23 @@
+class MaintenanceRecord:
+    def __init__(self, maintenance_id, equipment, fault_description, date_started, expected_completion_date, date_completed, technician_name, status, cost):
+        self.maintenance_id = maintenance_id
+        self.equipment = equipment
+        self.fault_description = fault_description
+        self.date_started = date_started
+        self.expected_completion_date = expected_completion_date
+        self.date_completed = date_completed
+        self.technician_name = technician_name
+        self.status = status
+        self.cost = cost
+
+    def start_maintenance(self):
+        pass
+
+    def update_status(self):
+        pass
+
+    def complete_maintenance(self):
+        pass
+
+    def display_record(self):
+        pass
