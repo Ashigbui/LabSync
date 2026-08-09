b09bf6fc80248ffe056da19d052a3e9fd9d41705
framzy-dot <afram.adwoaaboagyewaa@gmail.com>
2026-08-09 23:05:45 +0000

    Update fablab.py

--- src/fablab.py
+++ src/fablab.py
@@ -10,17 +10,39 @@ class Fablab:
     
     def add_equipment(self, equipment):
         self.equipment_list.append(equipment)
-        pass 
+        print(f"{equipment} added to {self.lab_name}.")
     
     def remove_equipment(self, equipment):
-        pass 
+        if equipment in self.equipment_list:
+            self.equipment_list.remove(equipment)
+            print(f"{equipment} removed from {self.equipment_list}.")
+        else:
+            print(f"{equipmnt} not found in {self.equipment_list}.")
     
     def search_equipment(self, equipment_name):
-        pass
-    
+        if equipment_name in self.equipment_list:
+            print("{equipment_name} is available in {self.equipment_list}.")
+            return True
+        else:
+            print("{equipment_name} not found in {self.equipment_list}.")
+            return False
+        
     def view_availability(self):
-        pass
+        if self.equipment_list:
+            print(f"Available equipment in {self.lab_name}:")
+            for equipment in self.equipment_list:
+                print(f"- {equipment}")
+        else:
+            print(f"No equipment currently available in {self.lab_name}.")
     
     def display_lab_info(self):
-        pass
+        print(f"""
+        Lab ID: {self.lab_id}
+        Lab Name: {self.lab_name}
+        Location: {self.location}
+        Technician: {self.technician}
+        Opening Hours: {self.opening_hours}
+        Closing Hours: {self.closing_hours}
+        Equipment List: {', '.join(self.equipment_list) if self.equipment_list else 'None'}
+        """)
     
